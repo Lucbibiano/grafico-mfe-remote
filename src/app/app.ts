800f4ce6ee6b97ec39f3f909995826b7e1972e64
Lucbibiano <lukasssb@hotmail.com>
2026-03-03 18:03:34 -0300

    implementing mfe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Chart, registerables } from 'chart.js/auto'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('grafico');
+export class App implements OnInit {
+  ngOnInit(): void {
+    const dados = [
+      { categoria: 'Eletronicos', valor: 10 },
+      { categoria: 'Roupas', valor: 50 },
+      { categoria: 'Decoração', valor: 5 },
+      { categoria: 'Ferramentas', valor: 15 },
+      { categoria: 'Acessórios', valor: 20 },
+      { categoria: 'Beleza', valor: 30 },
+    ];
+
+    const labels = dados.map(v => v.categoria);
+const valores = dados.map(v => v.valor);
+
+    new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Categorias Mais Vendidas',
+            data: valores,
+            backgroundColor: 'rgba(25, 90, 168, 0.6)'
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales:{
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    })
+  }
 }
